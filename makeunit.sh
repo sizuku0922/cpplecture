3c820c3c6b30e4f0e8755c731b74b1bf070937e1
tasksat <tasuku.s310@gmail.com>
2026-03-26 10:18:04 +0900

    minor update

--- makeunit.sh
+++ makeunit.sh
@@ -1,6 +1,27 @@
 #!/bin/sh
 
-for num in `seq $1 $2`; do
-  mkdir Unit$num
-  echo "" >> ./Unit$num/main.cpp
-done
+make_unit() {
+  num=$1
+  unit="Unit$num"
+  if [ ! -d "$unit" ]; then
+    mkdir -p "$unit"
+  fi
+  file="$unit/main.cpp"
+  if [ ! -e "$file" ]; then
+    touch "$file"
+  fi
+}
+
+if [ "$#" -eq 1 ]; then
+  make_unit "$1"
+elif [ "$#" -eq 2 ]; then
+  for i in $(seq "$1" "$2"); do
+    make_unit "$i"
+  done
+else
+  cat >&2 <<EOF
+Usage: $0 <N>        : creates Unit<N>
+       $0 <L> <R>    : creates Unit<L> ... Unit<R>
+EOF
+  exit 1
+fi
